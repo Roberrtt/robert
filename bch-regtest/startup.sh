c45c5f8804cd55e7f5b86fdd104d27e4428fa87f
root <patruk2014@gmail.com>
2020-10-29 07:00:53 -0700

    update

--- bch-regtest/startup.sh
+++ bch-regtest/startup.sh
@@ -1,23 +1,26 @@
-#!/bin/bash
+
 set -e
 
-if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1" == "test_bitcoin" ]]; then
+if [[ "$1" == "<%= binary_cli %>" || "$1" == "<%= binary_tx %>" || "$1" == "<%= binary %>" || "$1" == "<%= binary_test %>" ]]; then
 	mkdir -p "$BITCOIN_DATA"
 
 	if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
-		cat <<-EOF >"$BITCOIN_DATA/bitcoin.conf"
-			printtoconsole=1
-			rpcallowip=::/0
-			rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
-			rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+		cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
+		regtest=1
+		printtoconsole=1
+		rpcallowip=::/0
+		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
+		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
 		EOF
 		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
 	fi
-
-
+t
 	chown -R bitcoin "$BITCOIN_DATA"
 	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.bitcoin
 	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
+
+	exec gosu bitcoin "$@"
 fi
-bitcoind -chain=regtest  --datadir=/data --conf=/bitcoin.conf 
+
+exec "$@"
 
